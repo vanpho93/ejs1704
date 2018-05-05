edcfa24f1088141f231e72700c272a26b4941b60
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-05 20:48:15 +0700

    syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'));
 
-app.get('/syntax', (req, res) => res.render('syntax'));
+app.get('/syntax', (req, res) => res.render('syntax', { soA: 10 }));
 
 app.listen(3000, () => console.log('Server started!'));
 reload(app);
