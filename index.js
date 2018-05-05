6c33393569365cea4762a2b3454e749ca1984179
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-05 20:58:09 +0700

    words

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const reload = require('reload');
 
+const words = [
+    { id: 'a1', en: 'One', vn: 'Mot', isMemorized: false },
+    { id: 'a2', en: 'Two', vn: 'Hai', isMemorized: true },
+    { id: 'a3', en: 'Three', vn: 'Ba', isMemorized: true },
+    { id: 'a4', en: 'Four', vn: 'Bon', isMemorized: false },
+]
+
 const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'));
 
 app.get('/syntax', (req, res) => res.render('syntax', { soA: 10 }));
+
+app.get('/words', (req, res) => res.render('words', { words }));
 
 app.listen(3000, () => console.log('Server started!'));
 reload(app);
