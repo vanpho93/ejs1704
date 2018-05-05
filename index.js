f8dd575f340fab19be36dbc8cab8e246f11102a9
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-05 19:47:34 +0700

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.listen(3000, () => console.log('Server started!'));
