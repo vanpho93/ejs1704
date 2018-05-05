654fab363ba184d9f20ed671913bd317da53bef6
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-05 20:16:10 +0700

    package.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const reload = require('reload');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -6,3 +7,4 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
 
 app.listen(3000, () => console.log('Server started!'));
+reload(app);
